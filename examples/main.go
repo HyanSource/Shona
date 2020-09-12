a7bda7479cce2ba7c0dd57ef07ac88dcd41620d4
Hyan <s815651@yahoo.com.tw>
2020-09-13 00:48:44 +0800

    examples

--- examples/main.go
+++ examples/main.go
@@ -1,34 +1,43 @@
 package main
 
+/*
+ * 以LineGame做使用示範 計算出RTP以及免費觸發機率
+ *
+ */
+
+import (
+	"fmt"
+
+	"github.com/HyanSource/Shona/ISlot"
+	"github.com/HyanSource/Shona/Slot"
+	"github.com/HyanSource/Shona/st"
+)
+
 func main() {
+	//新增滾輪條
+	rs := []ISlot.IRow{
+		Slot.NewRow([]string{"K", "Q", "J", "10", "9", "9", "K", "Q", "J", "10", "9", "10", "Q", "J", "10", "9", "9", "J", "10", "9"}),
+		Slot.NewRow([]string{"W", "K", "Q", "J", "10", "9", "S", "K", "Q", "J", "10", "9", "K", "Q", "J", "10", "9", "J", "J", "9"}),
+		Slot.NewRow([]string{"W", "K", "Q", "J", "10", "9", "S", "K", "Q", "J", "10", "9", "K", "Q", "J", "10", "9", "J", "10", "9"}),
+		Slot.NewRow([]string{"W", "K", "Q", "J", "10", "9", "S", "K", "Q", "J", "10", "9", "K", "Q", "J", "10", "9", "J", "10", "9"}),
+		Slot.NewRow([]string{"W", "K", "Q", "J", "10", "9", "9", "K", "Q", "J", "10", "9", "K", "Q", "J", "10", "9", "J", "10", "9"}),
+	}
 
-	// //素材管理
-	// sm := Slot.NewSymbolManage()
-	// sm.Add(Slot.NewSymbol("0", Slot.NORMAL, []float64{0, 0, 5, 10, 30}))
-	// sm.Add(Slot.NewSymbol("1", Slot.NORMAL, []float64{0, 0, 5, 10, 30}))
-	// sm.Add(Slot.NewSymbol("2", Slot.NORMAL, []float64{0, 0, 5, 10, 30}))
-	// sm.Add(Slot.NewSymbol("3", Slot.NORMAL, []float64{0, 0, 10, 15, 50}))
-	// sm.Add(Slot.NewSymbol("4", Slot.NORMAL, []float64{0, 0, 10, 15, 50}))
-	// sm.Add(Slot.NewSymbol("5", Slot.NORMAL, []float64{0, 0, 15, 50, 120}))
-	// sm.Add(Slot.NewSymbol("6", Slot.NORMAL, []float64{0, 0, 15, 50, 120}))
-	// sm.Add(Slot.NewSymbol("7", Slot.NORMAL, []float64{0, 0, 25, 75, 300}))
-	// sm.Add(Slot.NewSymbol("8", Slot.NORMAL, []float64{0, 0, 25, 75, 300}))
-	// sm.Add(Slot.NewSymbol("9", Slot.NORMAL, []float64{0, 0, 50, 150, 1000}))
-	// sm.Add(Slot.NewSymbol("10", Slot.WILD, []float64{0, 0, 0, 0, 0}))
-	// sm.Add(Slot.NewSymbol("11", Slot.SCATTER, []float64{0, 0, 0, 0, 0}))
+	//新增元素類型以及賠率
+	sm := Slot.NewSymbolManage()
+	sm.Add(Slot.NewSymbol("K", st.NORMAL, []float64{0, 0, 5, 30, 100}))
+	sm.Add(Slot.NewSymbol("Q", st.NORMAL, []float64{0, 0, 5, 25, 100}))
+	sm.Add(Slot.NewSymbol("J", st.NORMAL, []float64{0, 0, 5, 20, 75}))
+	sm.Add(Slot.NewSymbol("10", st.NORMAL, []float64{0, 0, 5, 20, 75}))
+	sm.Add(Slot.NewSymbol("9", st.NORMAL, []float64{0, 0, 5, 10, 50}))
+	sm.Add(Slot.NewSymbol("S", st.SCATTER, []float64{0, 0, 50, 0, 0}))
+	sm.Add(Slot.NewSymbol("W", st.WILD, []float64{0, 0, 0, 0, 0}))
 
-	// //新增行
-	// r := []Slot.IRow{
-	// 	Slot.NewRow([]string{}),
-	// 	Slot.NewRow([]string{}),
-	// 	Slot.NewRow([]string{}),
-	// 	Slot.NewRow([]string{}),
-	// 	Slot.NewRow([]string{}),
-	// }
+	//新增LineGame計算
+	cl := Slot.NewCalcLine(3, rs, sm)
+	cl.Init()
 
-	// //盤面物件
-	// rc := Slot.NewRowsControll(r, sm, 3)
-	// rc.GetLineGameRTP()
-
-	// fmt.Println("OK")
+	result := cl.GetResult()
+	fmt.Println("RTP:", result.GetRTP())
+	fmt.Println("FreeP:", result.GetFreeP())
 }
